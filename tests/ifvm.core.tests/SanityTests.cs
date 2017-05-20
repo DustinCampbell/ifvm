32ef653f530edb6f283ae1cdaf5197608d17d57c
Dustin Campbell <dustin@teamcampbell.org>
2017-05-20 10:13:01 -0700

    Initial project structure

---
+++ tests/ifvm.core.tests/SanityTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace IFVM.Core.Tests
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void true_is_true()
+        {
+            Assert.True(true, "If this fails, all hope is lost.");
+        }
+    }
+}
