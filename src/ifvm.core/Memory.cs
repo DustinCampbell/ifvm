804b7ddd5e0d5d55521c6335a2432c9dc8364786
Dustin Campbell <dustin@teamcampbell.org>
2017-08-05 05:26:01 -0700

    Add object pool and use it to make reads and writes atomic

--- src/ifvm.core/Memory.cs
+++ src/ifvm.core/Memory.cs
@@ -4,6 +4,9 @@ namespace IFVM.Core
 {
     public class Memory
     {
+        private static readonly ObjectPool<byte[]> s_twoByteArrays = new ObjectPool<byte[]>(() => new byte[2], 512);
+        private static readonly ObjectPool<byte[]> s_fourByteArrays = new ObjectPool<byte[]>(() => new byte[4], 512);
+
         private readonly byte[] _bytes;
 
         public int Length => _bytes.Length;
@@ -34,8 +37,13 @@ namespace IFVM.Core
         {
             ValidateIndex(index, sizeof(ushort));
 
-            var b1 = _bytes[index];
-            var b2 = _bytes[index + 1];
+            var tempArray = s_twoByteArrays.Allocate();
+            Array.Copy(_bytes, index, tempArray, 0, sizeof(ushort));
+
+            var b1 = tempArray[0];
+            var b2 = tempArray[1];
+
+            s_twoByteArrays.Free(tempArray);
 
             return (ushort)((b1 << 8) | b2);
         }
@@ -44,10 +52,15 @@ namespace IFVM.Core
         {
             ValidateIndex(index, sizeof(uint));
 
-            var b1 = _bytes[index];
-            var b2 = _bytes[index + 1];
-            var b3 = _bytes[index + 2];
-            var b4 = _bytes[index + 3];
+            var tempArray = s_fourByteArrays.Allocate();
+            Array.Copy(_bytes, index, tempArray, 0, sizeof(uint));
+
+            var b1 = tempArray[0];
+            var b2 = tempArray[1];
+            var b3 = tempArray[2];
+            var b4 = tempArray[3];
+
+            s_fourByteArrays.Free(tempArray);
 
             return (uint)((b1 << 24) | (b2 << 16) | (b3 << 8) | b4);
         }
@@ -63,18 +76,30 @@ namespace IFVM.Core
         {
             ValidateIndex(index, sizeof(ushort));
 
-            _bytes[index] = (byte)(value >> 8);
-            _bytes[index + 1] = (byte)(value & 0x00ff);
+            var tempArray = s_twoByteArrays.Allocate();
+
+            tempArray[0] = (byte)(value >> 8);
+            tempArray[1] = (byte)(value & 0x00ff);
+
+            Array.Copy(tempArray, 0, _bytes, index, sizeof(ushort));
+
+            s_twoByteArrays.Free(tempArray);
         }
 
         public void WriteDWord(int index, uint value)
         {
             ValidateIndex(index, sizeof(uint));
 
-            _bytes[index] = (byte)(value >> 24);
-            _bytes[index + 1] = (byte)(value >> 16);
-            _bytes[index + 2] = (byte)(value >> 8);
-            _bytes[index + 3] = (byte)(value & 0x000000ff);
+            var tempArray = s_fourByteArrays.Allocate();
+
+            tempArray[0] = (byte)(value >> 24);
+            tempArray[1] = (byte)(value >> 16);
+            tempArray[2] = (byte)(value >> 8);
+            tempArray[3] = (byte)(value & 0x000000ff);
+
+            Array.Copy(tempArray, 0, _bytes, index, sizeof(uint));
+
+            s_fourByteArrays.Free(tempArray);
         }
     }
 }
---
+++ src/ifvm.core/ObjectPool.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+
+namespace IFVM.Core
+{
+    public class ObjectPool<T> where T : class
+    {
+        private struct Item
+        {
+            public T Value;
+        }
+
+        private T _firstItem;
+        private readonly Item[] _items;
+        private readonly Func<T> _createItem;
+
+        public ObjectPool(Func<T> createItem)
+            : this(createItem, Environment.ProcessorCount * 2)
+        {
+        }
+
+        public ObjectPool(Func<T> createItem, int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size));
+            }
+
+            _createItem = createItem ?? throw new ArgumentNullException(nameof(createItem));
+            _items = new Item[size - 1];
+        }
+
+        public T Allocate()
+        {
+            // First, examine the first item. If that fails, call AllocateSlow to search the remaining items.
+            // Note that the read below is optimistically not synchronized. The idea here is to only interlock when
+            // we actually have a candidate. In the worst case, we may miss some recently returned objects,
+            // which is acceptable.
+            var result = _firstItem;
+
+            if (result == null || result != Interlocked.CompareExchange(ref _firstItem, null, result))
+            {
+                result = AllocateSlow();
+            }
+
+            return result;
+        }
+
+        private T AllocateSlow()
+        {
+            var items = _items;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                // Note that the read below is optimistically not synchronized. The idea here is to only interlock when
+                // we actually have a candidate. In the worst case, we may miss some recently returned objects,
+                // which is acceptable.
+                var result = items[i].Value;
+
+                if (result != null && result == Interlocked.CompareExchange(ref items[i].Value, null, result))
+                {
+                    return result;
+                }
+            }
+
+            return _createItem();
+        }
+
+        public void Free(T obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (_firstItem == null)
+            {
+                // We intentionally don't use interlocked here. At worst, an object was stored in _firstItem
+                // after we checked it for null and that object will be collected.
+                _firstItem = obj;
+            }
+            else
+            {
+                FreeSlow(obj);
+            }
+        }
+
+        private void FreeSlow(T obj)
+        {
+            var items = _items;
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].Value == null)
+                {
+                    // We intentionally don't use interlocked here. At worst, an object was stored in items at this slot
+                    // after we checked it for null and that object will be collected.
+                    items[i].Value = obj;
+                    break;
+                }
+            }
+        }
+    }
+}
