715f4d20b377a99cf626c61118cc7dec17f35e22
Dustin Campbell <dustin@teamcampbell.org>
2017-08-11 07:59:49 -0700

    Add ifvm.testutilities project with games as embedded resources

--- tests/ifvm.core.tests/SanityTests.cs
+++ tests/ifvm.core.tests/SanityTests.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using IFVM.TestUtilities;
+using Xunit;
 
 namespace IFVM.Core.Tests
 {
@@ -9,6 +10,15 @@ namespace IFVM.Core.Tests
         {
             Assert.True(true, "If this fails, all hope is lost.");
             Assert.False(false, "If this fails, all hope is lost.");
+        }
+
+        [Fact]
+        public void load_resource()
+        {
+            using (var stream = Resources.LoadResource(Resources.Glulx_Advent))
+            {
+                Assert.NotNull(stream);
+            }
         }
     }
 }
---
+++ tests/ifvm.testutilities/Resources.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace IFVM.TestUtilities
+{
+    public static class Resources
+    {
+        public const string Glulx_Advent = "advent.ulx";
+        public const string Glulx_Glulxercise = "glulxercise.ulx";
+
+        public static Stream LoadResource(string resourceName)
+        {
+            return typeof(Resources).GetTypeInfo().Assembly.GetManifestResourceStream($"IFVM.TestUtilities.Resources.{resourceName}");
+        }
+    }
+}
